1e3e486e4c4d654d5a533c2e6b4c69dd24605a68
CLOPS_Muhammed_H <muhammed.hashir@cloudifyops.com>
2025-07-23 11:53:01 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source              = "../modules/vpc"
+  vpc_cidr            = var.vpc_cidr
+  vpc_name            = var.vpc_name
+  public_subnet_cidr  = var.public_subnet_cidr
+  public_subnet_name  = var.public_subnet_name
+  availability_zone   = var.availability_zone
+}
+
+module "ec2" {
+  source         = "../modules/ec2"
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  subnet_id      = module.vpc.public_subnet_id
+  instance_name  = var.instance_name
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "public_subnet_name" {
+  description = "Name tag for the public subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the public subnet"
+  type        = string
+} 
